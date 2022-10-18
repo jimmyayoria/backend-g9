d087aec7edc4423567aa6e2d176877b358160c5e
jimmyayoria <jimmyayoriakiara@gmail.com>
2022-10-18 10:27:19 -0500

    agregando los 2 primeros archivos de python

---
+++ dia-1/2-impresion-pantalla.py
@@ -0,0 +1,21 @@
+correo='jimmyayoriakiara@gmail.com'
+estatura=1.70
+print(correo)
+estatura
+
+#juntar 2 textos, al momento de poner la coma se agregara un espacio al imprimir
+print('El Correo del usuario es:',correo)
+
+# si usamos el (+) concatenara los textos pero sin agregar el esapcio
+print('El correo del usuario es:'+correo)
+
+# la concatenacion por el (+) solamente se hace entre STRINGS
+print('La estatura del usuario es:',(estatura))
+
+# Parsear > convertir un tipo de dato a otro
+print('La estatura del usuario es:'+str(estatura))
+#str(var) > convierte el tipo de dato del proveniente de la variable a un string
+#int(var) > conviertira a entero
+#float(var) > conviertira a float ...
+
+
