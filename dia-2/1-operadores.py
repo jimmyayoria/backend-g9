974306fdc388ce875c76164dd090e95560c3aaf0
jimmyayoria <jimmyayoriakiara@gmail.com>
2022-10-19 19:23:12 -0500

    ejercicios_dia2

--- dia-2/1-operadores.py
+++ dia-2/1-operadores.py
@@ -64,7 +64,66 @@ print(numero1) # float
 print(numero2) # Int
 print(numero1 == numero2)
 print(int(40.7)) # le quita el decimal
+# print(int(eduardo)) # no se puede convertir tipos de datos irreales
 print(type(numero1) == type(numero2))
+
+# MAYOR / MAYOR O IGUAL
+print(10 > 9.58)
+print(10 > int('5'))
+print(50 >= 30)
+
+# MENOR / MENOR O IGUAL
+print(50 < 80)
+print(50 <= 50)
+# siempre va el simbolo de mayor o menor antes que el igual, nunca al revez porque python entiende que se esta tratando de una asignacion
+print(100<= float('40.24'))
+print (100<=40.52)
+
+# --------------------------------------------
+# OPERADORES LOGICOS  --> Sirven para comparar varias condicionales
+# --------------------------------------------
+
+# && para indicar un AND , en python se usa la palabra AND
+# || para indicar un OR , en python se usa la palabra OR
+
+# and  ---> todas las condiciones deben ser verdaderas para que sea verdadero
+eduardo=30
+ronald=25
+henry=25
+carmen=19
+angel=15
+
+print((angel > eduardo) and (ronald < henry))
+
+print((eduardo > angel) and (carmen < ronald))
+
+# or -----> AL MENOS una condicion tiene que ser verdadera para que toda sea verdadero
+
+print((carmen > ronald) or (eduardo > ronald))
+
+# COMPUERTA LOGICA ( operador and)
+#  V1 |  V2  |  R
+#  F  |   F  |  F
+#  V  |   F  |  F
+#  F  |   V  |  F
+#  V  |   V  |  V
+
+# COMPUERTA LOGICA (Operador or)
+#  V1 |  V2  |  R
+#  F  |   F  |  F
+#  V  |   F  |  V
+#  F  |   V  |  V
+#  V  |   V  |  V
+
+# --------------------------------------------
+# OPERADORES DE IDENTIDAD  --> Sirven para comparar varias condicionales
+# ------------------------------------------
+
+verduras = ['apio','rocoto','zanahoria']
+print('rocoto' in verduras)
+print('champiñon' not in verduras)
+print(('apio','rocoto') in verduras)
+
 
 
 
